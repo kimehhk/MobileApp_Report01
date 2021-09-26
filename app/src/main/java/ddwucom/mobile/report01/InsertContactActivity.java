6a540485fd98e620f4834fc5f2312489661f48d9
kimehhk <kimehhk@gmail.com>
2021-09-26 18:01:07 +0900

    insert db edit

--- app/src/main/java/ddwucom/mobile/report01/InsertContactActivity.java
+++ app/src/main/java/ddwucom/mobile/report01/InsertContactActivity.java
@@ -2,10 +2,12 @@ package ddwucom.mobile.report01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class InsertContactActivity extends AppCompatActivity {
 
@@ -14,6 +16,7 @@ public class InsertContactActivity extends AppCompatActivity {
     EditText etContent;
 
     ContactDBHelper helper;
+    Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +29,22 @@ public class InsertContactActivity extends AppCompatActivity {
 
         helper = new ContactDBHelper(this);
 
+        /*SQLiteDatabase db = helper.getReadableDatabase();
+        cursor = db.rawQuery("SELECT _id FROM " + ContactDBHelper.TABLE_NAME, null);
+        cursor.moveToLast();
+
+        int id = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactDBHelper.COL_ID))) + 1;
+
+        Toast.makeText(InsertContactActivity.this, Integer.toString(id), Toast.LENGTH_SHORT).show();
+        helper.close();*/
+
     }
 
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_insertComplete:
                 insertPost();
+                finish();
                 break;
             case R.id.btn_insertCancel:
                 finish();
@@ -40,9 +53,26 @@ public class InsertContactActivity extends AppCompatActivity {
     }
 
     public void insertPost() {
-        SQLiteDatabase db = helper.getWritableDatabase();
-        db.execSQL("INSERT INTO " + ContactDBHelper.TABLE_NAME);
+        SQLiteDatabase db = helper.getReadableDatabase();
+        cursor = db.rawQuery("SELECT _id FROM " + ContactDBHelper.TABLE_NAME, null);
+        cursor.moveToLast();
+
+        int id = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactDBHelper.COL_ID))) + 1;
+        String title = etTitle.getText().toString();
+        String artist = etArtist.getText().toString();
+        String content = etContent.getText().toString();
+
+
+        db.execSQL("INSERT INTO " + ContactDBHelper.TABLE_NAME + " VALUES (" + Integer.toString(id) + ", 'user', '" + title + "', '" + artist + "', '" + content + "');");
+
         helper.close();
+        cursor.close();
+    }
+
+    protected void onDestory() {
+        super.onDestroy();
+        if(cursor != null)
+            cursor.close();
     }
 
 }
